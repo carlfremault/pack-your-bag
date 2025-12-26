2311f5bc9ddb45183a2191fa5906190c2aacc853
carlfremault <carlfremault@yahoo.com>
2025-12-26 12:32:06 +0000

    import cleanup

--- apps/user-data-service/eslint.config.mjs
+++ apps/user-data-service/eslint.config.mjs
@@ -1,11 +1,9 @@
 import { getNestJsConfig } from '@repo/eslint-config/nest-js';
 
-const nestJsConfig = getNestJsConfig(import.meta.dirname);
-
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   {
     ignores: ['eslint.config.mjs', 'dist/**', 'coverage/**'],
   },
-  ...nestJsConfig,
+  ...getNestJsConfig(import.meta.dirname),
 ];
