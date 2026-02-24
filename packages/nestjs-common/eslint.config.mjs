c14dea3cd2dc273d70b2f6c5fd0b8da7bf55ee22
carlfremault <carlfremault@yahoo.com>
2026-02-24 15:52:20 +0000

    [Shared] moved BffGuard to nestjs-common package - esling addendum

--- packages/nestjs-common/eslint.config.mjs
+++ packages/nestjs-common/eslint.config.mjs
@@ -3,7 +3,7 @@ import { getNestJsConfig } from '@repo/eslint-config/nest-js';
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   {
-    ignores: ['eslint.config.mjs', 'dist/**'],
+    ignores: ['eslint.config.mjs', 'dist/**', 'coverage/**'],
   },
   ...getNestJsConfig(import.meta.dirname),
 ];
