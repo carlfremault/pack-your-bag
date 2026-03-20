6656b3c722b05ffec0a683720a9887dacbdcccf2
carlfremault <carlfremault@yahoo.com>
2026-03-20 15:02:29 +0000

    [UDS] httpclient package and generation

---
+++ packages/user-data-client/src/index.ts
@@ -0,0 +1,17 @@
+import type { paths } from './schema';
+
+export type { paths } from './schema';
+
+export type SuccessResponse<P extends keyof paths, M extends keyof paths[P]> = paths[P][M] extends {
+  responses: { 200: { content: { 'application/json': infer R } } };
+}
+  ? R
+  : paths[P][M] extends { responses: { 201: { content: { 'application/json': infer R } } } }
+    ? R
+    : never;
+
+export type RequestBody<P extends keyof paths, M extends keyof paths[P]> = paths[P][M] extends {
+  requestBody: { content: { 'application/json': infer R } };
+}
+  ? R
+  : never;
