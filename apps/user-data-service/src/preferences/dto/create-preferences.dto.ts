26a23a7496e20e34b2ee7593c36949d351b4799e
carlfremault <carlfremault@yahoo.com>
2026-03-20 14:44:07 +0000

    [UDS] DTO IsEnum validator

--- apps/user-data-service/src/preferences/dto/create-preferences.dto.ts
+++ apps/user-data-service/src/preferences/dto/create-preferences.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import { IsIn, IsOptional, IsString } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 import { DateFormat, Theme, TimeFormat, Units } from '../types/preferences.types';
 
@@ -8,24 +8,24 @@ export class CreatePreferencesDto {
   @ApiProperty({ description: 'Units', example: 'metric' })
   @IsString()
   @IsOptional()
-  @IsIn(Object.values(Units))
+  @IsEnum(Units)
   units: Units;
 
   @ApiProperty({ description: 'Theme', example: 'light' })
   @IsString()
   @IsOptional()
-  @IsIn(Object.values(Theme))
+  @IsEnum(Theme)
   theme: Theme;
 
   @ApiProperty({ description: 'Date format', example: 'DD/MM/YYYY' })
   @IsString()
   @IsOptional()
-  @IsIn(Object.values(DateFormat))
+  @IsEnum(DateFormat)
   dateFormat: DateFormat;
 
   @ApiProperty({ description: 'Time format', example: '12h' })
   @IsString()
   @IsOptional()
-  @IsIn(Object.values(TimeFormat))
+  @IsEnum(TimeFormat)
   timeFormat: TimeFormat;
 }
