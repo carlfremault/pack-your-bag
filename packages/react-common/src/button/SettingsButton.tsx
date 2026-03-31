869b9c1fad187c2de87edbca811c2aa485b08cbd
carlfremault <carlfremault@yahoo.com>
2026-03-31 13:45:28 +0000

    [React-common] DesktopHeader; SettingsButton; Logo

---
+++ packages/react-common/src/button/SettingsButton.tsx
@@ -0,0 +1,17 @@
+import { MdOutlineSettings } from 'react-icons/md';
+
+import Button from './Button';
+
+export interface SettingsButtonProps {
+  onClick: () => void;
+}
+
+export default function SettingsButton(props: SettingsButtonProps) {
+  const { onClick } = props;
+
+  return (
+    <Button onClick={onClick} variant="ghost" color="primary" aria-label="Settings">
+      <MdOutlineSettings className="h-5 w-5" />
+    </Button>
+  );
+}
---
+++ packages/react-common/src/header/DesktopHeader.tsx
@@ -0,0 +1,20 @@
+import SettingsButton from '../button/SettingsButton';
+import DesktopNavButtons from '../navigation/DesktopNavButtons';
+import { NavTab } from '../navigation/types';
+
+export interface DesktopHeaderProps {
+  activeTab: NavTab;
+  onTabChange: (tab: NavTab) => void;
+  onSettingsClick: () => void;
+}
+
+export default function DesktopHeader(props: DesktopHeaderProps) {
+  const { activeTab, onTabChange, onSettingsClick } = props;
+
+  return (
+    <div className="border-primary-ring bg-background z-10 flex items-center justify-between border-b px-4 py-3 shadow-sm">
+      <DesktopNavButtons activeTab={activeTab} onTabChange={onTabChange} />
+      <SettingsButton onClick={onSettingsClick} />
+    </div>
+  );
+}
