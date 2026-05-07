617e46dc150c0f4d0adf044af2466beb658d9c98
carlfremault <carlfremault@yahoo.com>
2026-05-07 10:32:39 +0000

    [Web] UI tweak : flex correction

--- apps/web/src/components/Sidebar/ActionPanel.tsx
+++ apps/web/src/components/Sidebar/ActionPanel.tsx
@@ -132,11 +132,9 @@ function ActionPanelInner() {
     return (
       <SidebarPortal>
         {panelContent ? (
-          <div className="flex h-full">
-            <div className="flex min-h-0 flex-1 flex-col justify-center gap-4">
-              {desktopTitle && <h2 className="text-primary text-xl">{desktopTitle}</h2>}
-              {panelContent}
-            </div>
+          <div className="flex min-h-0 flex-1 flex-col justify-center gap-4">
+            {desktopTitle && <h2 className="text-primary text-xl">{desktopTitle}</h2>}
+            {panelContent}
           </div>
         ) : (
           <SidebarNav pathname={pathname} />
