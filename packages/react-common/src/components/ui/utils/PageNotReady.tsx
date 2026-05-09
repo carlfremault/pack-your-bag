277d99a575e1a10a2b57e7b484a20a0e81df9edf
carlfremault <carlfremault@yahoo.com>
2026-05-09 14:10:10 +0000

    [Web] PageNotReady not used anymore

--- packages/react-common/src/components/ui/utils/PageNotReady.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from '../spinner/Spinner';
-
-export function PageNotReady() {
-  return (
-    <div className="flex h-64 w-full items-center justify-center">
-      <Spinner size="large" />
-    </div>
-  );
-}
--- packages/react-common/src/components/ui/utils/index.ts
+++ packages/react-common/src/components/ui/utils/index.ts
@@ -2,6 +2,5 @@ export { CheckedWrapper } from './CheckedWrapper';
 export { DangerWrapper } from './DangerWrapper';
 export { ExpandableText } from './ExpandableText';
 export { MessageList } from './MessageList';
-export { PageNotReady } from './PageNotReady';
 export { FormNotReady } from './FormNotReady';
 export { SectionNotReady } from './SectionNotReady';
