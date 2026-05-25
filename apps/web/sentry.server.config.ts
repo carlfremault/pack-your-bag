6ed69f0ff54e2a2a10ccb38565826d9dcbc47df6
carlfremault <carlfremault@yahoo.com>
2026-05-25 14:45:39 +0000

    [Web] Web Sentry should also end up in Dev Sentry project during development

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -1,7 +1,12 @@
 import * as Sentry from '@sentry/nextjs';
 
+const isProduction = process.env.NODE_ENV === 'production';
+const dsn = isProduction
+  ? process.env.NEXT_PUBLIC_SENTRY_DSN
+  : process.env.NEXT_PUBLIC_DEV_SENTRY_DSN;
+
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn,
   environment: process.env.NODE_ENV,
   tracesSampleRate: parseFloat(process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE ?? '0.1'),
 });
